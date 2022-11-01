2e9874325475a941352cf906163e50043124ff9d
Berra Ozgun <berraozgun7@gmail.com>
2022-11-01 13:03:18 -0700

    hi

---
+++ car wash.py
@@ -0,0 +1,15 @@
+def wash_car(amount_payed):
+    if amount_payed == 12:
+        print ("You selected Platinum Wash!")
+        print ("Tricolor foam")
+        print ("Two rinses")
+        print ("Blow dry")
+
+    if amount_payed == 6:
+        print ("You selected Basic Wash!")
+        print ("White foam")
+        print ("One rinse")
+        print ("Drip dry")
+
+wash_car(12)
+
