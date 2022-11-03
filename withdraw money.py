7e46ed43a1d67b36ffbd29b7aa00d27143ca12a5
Berra Ozgun <berraozgun7@gmail.com>
2022-11-03 13:01:03 -0700

    hi

---
+++ withdraw money.py
@@ -0,0 +1,16 @@
+
+print ("Welcome! You have $100 in your account.")
+def withdraw_money(current_balance, amount):
+    
+    if (current_balance >= amount):
+        current_balance = current_balance - amount
+        print ("Your current balance is", current_balance)
+   
+    else:
+        print ("Error. You do not have that much money in your account")
+    
+amount=int(input ("How much do you want to withdraw? "))
+
+withdraw_money (100, amount)
+    
+   
